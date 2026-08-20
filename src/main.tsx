4c341e8e06ea16acce4d9321f08f09d5b08589e9
Xyruz Reyn Aninon <sibatmanaymaitim@gmail.com>
2026-08-20 16:13:30 +0800

    feat(ui): activate final 47i polish layer

--- src/main.tsx
+++ src/main.tsx
@@ -17,6 +17,7 @@ import './styles/staff-portal-47e.css'
 import './styles/dentist-portal-47f.css'
 import './styles/admin-portal-47g.css'
 import './styles/super-admin-portal-47h.css'
+import './styles/final-polish-47i.css'
 import App from './App.tsx'
 import { registerPatientPortalPwa } from './features/patientPortal/pwaRegistration'
 
