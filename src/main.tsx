2eef5cf69851a88f7c5571bd54e09a181068ea59
Xyruz Reyn Aninon <sibatmanaymaitim@gmail.com>
2026-08-20 14:44:05 +0800

    style(expenses): load expense modal styles

--- src/main.tsx
+++ src/main.tsx
@@ -7,6 +7,7 @@ import './styles/release-polish.css'
 import './styles/modal-fixes.css'
 import './styles/payment-recorder.css'
 import './styles/inventory-actions.css'
+import './styles/expense-actions.css'
 import App from './App.tsx'
 import { registerPatientPortalPwa } from './features/patientPortal/pwaRegistration'
 
