b5abc1a76e4a11333e8881bb1779b36a68653da8
Xyruz Reyn Aninon <sibatmanaymaitim@gmail.com>
2026-08-20 12:41:48 +0800

    Part 42: enable patient portal PWA registration

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,9 @@ import './index.css'
 import './styles/billing.css'
 import './styles/treatments.css'
 import App from './App.tsx'
+import { registerPatientPortalPwa } from './features/patientPortal/pwaRegistration'
+
+registerPatientPortalPwa()
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
