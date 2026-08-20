7d033a24dab5af5743571c4a04b4d71db159aad0
Xyruz Reyn Aninon <sibatmanaymaitim@gmail.com>
2026-08-21 07:53:48 +0800

    Load V30 settings redesign styles

--- src/main.tsx
+++ src/main.tsx
@@ -51,6 +51,7 @@ import './styles/super-admin-team-access-v26.css'
 import './styles/super-admin-branches-v27.css'
 import './styles/super-admin-forms-consent-v28.css'
 import './styles/super-admin-system-admin-v29.css'
+import './styles/super-admin-settings-v30.css'
 import App from './App.tsx'
 import { registerPatientPortalPwa } from './features/patientPortal/pwaRegistration'
 
