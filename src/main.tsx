bff3d266755432615a23a6fac5a811c3c22856db
Xyruz Reyn Aninon <sibatmanaymaitim@gmail.com>
2026-08-20 16:02:08 +0800

    feat(admin): load admin portal redesign styles

--- src/main.tsx
+++ src/main.tsx
@@ -15,6 +15,7 @@ import './styles/auth-47c.css'
 import './styles/patient-portal-47d.css'
 import './styles/staff-portal-47e.css'
 import './styles/dentist-portal-47f.css'
+import './styles/admin-portal-47g.css'
 import App from './App.tsx'
 import { registerPatientPortalPwa } from './features/patientPortal/pwaRegistration'
 
