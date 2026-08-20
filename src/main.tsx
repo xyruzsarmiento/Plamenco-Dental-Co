33f4b389be9753ebb4d1b7ee4ca0f485c3765c27
Xyruz Reyn Aninon <sibatmanaymaitim@gmail.com>
2026-08-21 07:37:49 +0800

    Load V27 branch workspace styles

--- src/main.tsx
+++ src/main.tsx
@@ -48,6 +48,7 @@ import './styles/super-admin-expenses-v23.css'
 import './styles/super-admin-communications-v24.css'
 import './styles/super-admin-notifications-v25.css'
 import './styles/super-admin-team-access-v26.css'
+import './styles/super-admin-branches-v27.css'
 import App from './App.tsx'
 import { registerPatientPortalPwa } from './features/patientPortal/pwaRegistration'
 
