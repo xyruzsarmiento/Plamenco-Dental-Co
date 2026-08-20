ac7cf2abd53bf36d5c15bb53fc06afcfe8fa061e
Xyruz Reyn Aninon <sibatmanaymaitim@gmail.com>
2026-08-21 07:59:19 +0800

    Add V31 appointments workspace wrapper

---
+++ src/pages/AppointmentsPageV31.tsx
@@ -0,0 +1,9 @@
+import { AppointmentsPage } from './AppointmentsPage'
+
+export function AppointmentsPageV31() {
+  return (
+    <div className="sa-appointments-v31">
+      <AppointmentsPage />
+    </div>
+  )
+}
