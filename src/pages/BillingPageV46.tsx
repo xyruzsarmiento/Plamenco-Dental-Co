d4fac66881a23aa7e4adf2678ba34bc3dc868bec
Xyruz Reyn Aninon <sibatmanaymaitim@gmail.com>
2026-08-21 16:21:38 +0800

    Add premium Billing workspace shell

---
+++ src/pages/BillingPageV46.tsx
@@ -0,0 +1,9 @@
+import { BillingPageV32 } from './BillingPageV32'
+
+export function BillingPageV46() {
+  return (
+    <section className="bill46-page">
+      <BillingPageV32 />
+    </section>
+  )
+}
