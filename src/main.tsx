e26f58e78e5bb8138e0f151f499467020ee4ec71
Xyruz Reyn Aninon <sibatmanaymaitim@gmail.com>
2026-08-21 16:03:54 +0800

    Load Treatments V43 premium styles

--- src/main.tsx
+++ src/main.tsx
@@ -63,6 +63,7 @@ import './styles/appointments-premium-v39.css'
 import './styles/appointments-premium-v40.css'
 import './styles/appointments-responsive-v41.css'
 import './styles/appointments-calendar-responsive-v42.css'
+import './styles/super-admin-treatments-v43.css'
 import App from './App.tsx'
 import { registerPatientPortalPwa } from './features/patientPortal/pwaRegistration'
 
