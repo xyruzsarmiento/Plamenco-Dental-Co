ef99a99020c61d71bf638d036ba5d8c03f563e87
Xyruz Reyn Aninon <sibatmanaymaitim@gmail.com>
2026-08-22 06:51:53 +0800

    Load dentist control refinements

--- src/main.tsx
+++ src/main.tsx
@@ -86,6 +86,7 @@ import './styles/clinical-record-detail-v56.css'
 import './styles/super-admin-services-actions-v56.css'
 import './styles/clinical-record-detail-v60.css'
 import './styles/super-admin-reports-v56.css'
+import './styles/super-admin-dentists-controls-v61.css'
 import App from './App.tsx'
 import { registerPatientPortalPwa } from './features/patientPortal/pwaRegistration'
 
