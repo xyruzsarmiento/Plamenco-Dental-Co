08a1f4e12a1e570246c5726c15ebfdd4eb68fd15
Xyruz Reyn Aninon <sibatmanaymaitim@gmail.com>
2026-08-21 14:41:52 +0800

    Stop destructive global database-to-local overwrite on refresh

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,9 @@
-import { useEffect } from 'react'
 import { AppRouter } from './app/AppRouter'
 import { AuthProvider } from './features/auth/AuthProvider'
 import { OfflineStatusBanner } from './features/patientPortal/OfflineStatusBanner'
 import { ModalAccessibilityManager } from './components/ui/ModalAccessibilityManager'
-import { syncSupabaseToLocalStorage } from './lib/supabaseSync'
 
 function App() {
-  useEffect(() => {
-    void syncSupabaseToLocalStorage()
-  }, [])
-
   return (
     <AuthProvider>
       <ModalAccessibilityManager />
