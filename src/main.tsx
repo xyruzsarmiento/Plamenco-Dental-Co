c98548c8df7cb082e0606007f34a65edd647d680
Xyruz Reyn Aninon <sibatmanaymaitim@gmail.com>
2026-08-22 05:49:19 +0800

    Load patient actions and modal redesign

--- src/main.tsx
+++ src/main.tsx
@@ -78,6 +78,7 @@ import './styles/super-admin-reports-analytics-v54.css'
 import './styles/super-admin-inventory-v55.css'
 import './styles/super-admin-inventory-v56.css'
 import './styles/super-admin-expenses-v56.css'
+import './styles/super-admin-patients-actions-v57.css'
 import App from './App.tsx'
 import { registerPatientPortalPwa } from './features/patientPortal/pwaRegistration'
 
