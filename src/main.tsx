8ae51d4da70751a809eb0383fb5b69627c9dc184
Xyruz Reyn Aninon <sibatmanaymaitim@gmail.com>
2026-08-22 07:53:45 +0800

    Load expense action and trend refinements

--- src/main.tsx
+++ src/main.tsx
@@ -94,6 +94,7 @@ import './styles/super-admin-expenses-v57.css'
 import './styles/super-admin-system-admin-v58.css'
 import './styles/super-admin-expenses-v65.css'
 import './styles/super-admin-settings-audit-v56.css'
+import './styles/super-admin-expenses-v58.css'
 import App from './App.tsx'
 import { registerPatientPortalPwa } from './features/patientPortal/pwaRegistration'
 
