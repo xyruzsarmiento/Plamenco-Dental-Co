4ee06c4d9e6cd0d62b0a29eb87597fa27d89f5ee
Xyruz Reyn Aninon <sibatmanaymaitim@gmail.com>
2026-08-21 16:09:30 +0800

    Create premium Treatment Plans V44 workspace

---
+++ src/pages/TreatmentPlansPageV44.tsx
@@ -0,0 +1,9 @@
+import { TreatmentPlansPageV13 } from './TreatmentPlansPageV13'
+
+export function TreatmentPlansPageV44() {
+  return (
+    <section className="treatment-plans-v44">
+      <TreatmentPlansPageV13 />
+    </section>
+  )
+}
