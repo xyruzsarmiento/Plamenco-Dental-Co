1687b18c32faa8a79b84a050fc86960764510df2
Xyruz Reyn Aninon <sibatmanaymaitim@gmail.com>
2026-08-21 20:59:04 +0800

    Load redesigned provider schedule styles

--- src/main.tsx
+++ src/main.tsx
@@ -73,6 +73,7 @@ import './styles/super-admin-services-v49.css'
 import './styles/super-admin-services-analytics-v50.css'
 import './styles/super-admin-dentists-v51.css'
 import './styles/super-admin-services-analytics-v52.css'
+import './styles/super-admin-dentists-schedule-v53.css'
 import App from './App.tsx'
 import { registerPatientPortalPwa } from './features/patientPortal/pwaRegistration'
 
