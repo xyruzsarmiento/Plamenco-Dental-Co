e87dfa050aedca6349ada112be016666637f0736
Xyruz Reyn Aninon <sibatmanaymaitim@gmail.com>
2026-08-22 08:51:17 +0800

    Load full-width expense trend refinement

--- src/main.tsx
+++ src/main.tsx
@@ -101,6 +101,7 @@ import './styles/super-admin-dashboard-responsive-v67.css'
 import './styles/super-admin-expenses-v68.css'
 import './styles/super-admin-expenses-v69.css'
 import './styles/super-admin-expenses-trend-v70.css'
+import './styles/super-admin-expenses-trend-v71.css'
 import App from './App.tsx'
 import { registerPatientPortalPwa } from './features/patientPortal/pwaRegistration'
 
