5b418e4ad4a73ca92d0235e4ab59ddb817f7fb2e
Xyruz Reyn Aninon <sibatmanaymaitim@gmail.com>
2026-08-22 09:16:06 +0800

    Load operating cost trend V74 styles

--- src/main.tsx
+++ src/main.tsx
@@ -104,6 +104,7 @@ import './styles/super-admin-expenses-trend-v70.css'
 import './styles/super-admin-expenses-trend-v71.css'
 import './styles/super-admin-expenses-trend-v72.css'
 import './styles/super-admin-expenses-trend-v73.css'
+import './styles/super-admin-expenses-trend-v74.css'
 import App from './App.tsx'
 import { registerPatientPortalPwa } from './features/patientPortal/pwaRegistration'
 
