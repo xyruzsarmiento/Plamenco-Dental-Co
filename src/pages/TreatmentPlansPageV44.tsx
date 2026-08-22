0e3dc719ecd80a2499b2d11e108e95acbc3b97b2
Xyruz Reyn Aninon <sibatmanaymaitim@gmail.com>
2026-08-22 10:58:55 +0800

    use redesigned treatment plan workspace

--- src/pages/TreatmentPlansPageV44.tsx
+++ src/pages/TreatmentPlansPageV44.tsx
@@ -1,9 +1,9 @@
-import { TreatmentPlansPageV13 } from './TreatmentPlansPageV13'
+import { TreatmentPlansPageV80 } from './TreatmentPlansPageV80'
 
 export function TreatmentPlansPageV44() {
   return (
     <section className="treatment-plans-v44">
-      <TreatmentPlansPageV13 />
+      <TreatmentPlansPageV80 />
     </section>
   )
 }
