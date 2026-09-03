d6aaf125120558498f7f0a90a5c3b3017501ecee
Xyruz Reyn Aninon <sibatmanaymaitim@gmail.com>
2026-09-04 01:07:05 +0800

    Wire Inventory V183 valuation and item management enhancer

--- src/pages/InventoryPageV56.tsx
+++ src/pages/InventoryPageV56.tsx
@@ -1,1 +1,18 @@
-export { InventoryPageV182 as InventoryPageV56 } from './InventoryPageV182'
+import { useEffect, useState } from 'react'
+import { InventoryEnhancerV183 } from '../components/system/InventoryEnhancerV183'
+import { InventoryPageV182 } from './InventoryPageV182'
+
+export function InventoryPageV56() {
+  const [revision, setRevision] = useState(0)
+
+  useEffect(() => {
+    const refresh = () => setRevision((value) => value + 1)
+    window.addEventListener('plamenco-inventory-updated', refresh)
+    return () => window.removeEventListener('plamenco-inventory-updated', refresh)
+  }, [])
+
+  return <>
+    <InventoryPageV182 key={revision} />
+    <InventoryEnhancerV183 onInventoryChanged={() => setRevision((value) => value + 1)} />
+  </>
+}
