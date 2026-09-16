149afd226dd9a43a44605d62924345839558404c
Xyruz Reyn Aninon <sibatmanaymaitim@gmail.com>
2026-09-16 21:08:35 +0800

    Wire inventory movement ledger

--- src/pages/InventoryPageV56.tsx
+++ src/pages/InventoryPageV56.tsx
@@ -1,6 +1,7 @@
 import { useEffect, useRef, useState } from 'react'
 import { InventoryBatchExpiryV227 } from '../components/system/InventoryBatchExpiryV227'
 import { InventoryEnhancerV183 } from '../components/system/InventoryEnhancerV183'
+import { InventoryMovementLedgerV228 } from '../components/system/InventoryMovementLedgerV228'
 import { InventoryTransferWorkflowV226 } from '../components/system/InventoryTransferWorkflowV226'
 import { InventoryWorkflowEnhancerV225 } from '../components/system/InventoryWorkflowEnhancerV225'
 import { useBranchContext } from '../features/branches/BranchContext'
@@ -100,5 +101,6 @@ export function InventoryPageV56() {
     <InventoryWorkflowEnhancerV225 onInventoryChanged={refreshWorkspace} />
     <InventoryTransferWorkflowV226 />
     <InventoryBatchExpiryV227 />
+    <InventoryMovementLedgerV228 />
   </>
 }
